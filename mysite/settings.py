2613799d5850699cf340fd293c5c182a95ab89c4
ksvoboda <k-svoboda@outlook.com>
2019-05-01 16:14:34 +0200

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -120,5 +120,11 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = '/home/site/wwwroot/static'
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
